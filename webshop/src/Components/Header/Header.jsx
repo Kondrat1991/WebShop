fbfd6a751dc813d8c138c7db51615b85356fd2df
Denis Minin <den11minin@gmail.com>
2018-05-29 10:28:46 +0300

    add component Header

---
+++ webshop/src/Components/Header/Header.jsx
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+// import Menu from '../Menu/Menu';
+// import Search from '../Search/Search';
+import './Header.css'
+
+
+const Header = () => (
+    <div className='wrapper'>
+        <div className="container">
+            <header className="header">
+                <button className="btn btn-menu"/>
+                <h1 className="title">
+                    maison
+                </h1>
+                {/*<Menu />*/}
+                <nav className='nav'>
+                    <ul className="menu">
+                        <li className="menu__item">Home</li>
+                        <li className="menu__item">Shop</li>
+                        <li className="menu__item">Favorite items</li>
+                    </ul>
+                </nav>
+                <div className='cart'>
+                <button className="btn btn-cart"/>
+                <div className="cart__counter">2</div>
+                    </div>
+            </header>
+        </div>
+    </div>
+);
+
+Header.propTypes = {};
+
+export default Header;
