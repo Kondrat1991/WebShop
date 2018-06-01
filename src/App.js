59606d0fae19e3671ce403dbec51bc859b71dd97
Denis Minin <den11minin@gmail.com>
2018-06-01 15:22:54 +0300

    modified app handle

--- src/App.js
+++ src/App.js
@@ -14,20 +14,18 @@ class App extends Component {
         basket: [],
     };
 
-    handler({search}) {
+    handler = (search) => {
         fetch(`https://www.googleapis.com/books/v1/volumes?q=${search}&orderBy=newest&langRestrict=en&download=epub&maxResults=40&filter=partial&startIndex=0&AIzaSyDZ_iy1QQ7PmcUf-Y3e1z7277ncsSf9GYE`)
             .then(result => result.json())
             .then(data => this.setState({
                 ...this.state,
                 library: data.items,
-                // compare: data.items.slice(0, 7), //  удалить перед мержем
             }))
             .then(data => console.log(this.state.library))
-            // .then(data => console.log(this.state.compare)) //  удалить перед мержем
-    }
+    };
 
     componentDidMount(){
-      this.handler('computers')
+      this.handler('music')
     }
 
     // универсальный метод, который добавляет/удаляет карточки в/из (пожеланий, сравнения, корзины) метод передать в
@@ -38,7 +36,7 @@ class App extends Component {
         if (!this.state[key].some((obj) => obj.id === id )) {
             const findId = this.state.library.find(el => el.id === id);
             this.setState((prevState) => ({
-                    [`${key}`]: [
+                    [key]: [
                         ...prevState[key], findId
                     ]
                 })
@@ -46,11 +44,10 @@ class App extends Component {
         } else {
             const filterArr = this.state[key].filter((obj) => obj.id !== id);
             this.setState({
-                [`${key}`]: filterArr,
+                [key]: filterArr,
             })
         }
     };
-
 
     render() {
         const {library, compare, wishList, basket} = this.state;
