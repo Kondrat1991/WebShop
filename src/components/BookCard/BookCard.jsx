53da9387cbd2877e42d271ccd9964c1671ff1dc0
Vova Melnyk <hagmen4339@gmail.com>
2018-06-04 16:35:13 +0300

    add card ui

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -7,43 +7,27 @@ const BookCard = ({title, image, price, author, addToCompareBooks, id, webReader
 
     return (
 
-
-
-
-
-
-        <div className="book-card">
-
-            <div className="book-img__container">
-                <img className="book-card__img"
-                     src={image}
-                     alt="#"/>
-
-                {/*<img className="book-icon" src="/heart.svg" alt="#"/>*/}
-                <h4 className="book-card__subtitle">{title}</h4>
-                <img className="book-icon" src="/heart.svg" alt="#"/>
-
-                <a className="book-link" href={webReaderLink} target='_blank'> </a>
-
-
-                <div className="book-button--container">
-                    <button className="book-button" type="submit"> </button>
-                    <p className="book-add">Add To Cart</p>
+        <div className="card">
+            <div className="card__cont">
+                <div className="card__overlay">
+                    <img className="book-icon" src="/like.svg" alt="#"/>
+                    <img className="book-comparison" src="/balance.svg" alt="#"/>
+                    <a className='preview' target={'_blank'} href={webReaderLink}>preview</a>
+                    <div className="buy">
+                        {/*<p className='cart'>Add to cart </p>*/}
+                        {/*<img src="/icon.svg" alt="#" className='card__info'/>*/}
+                        <img src="/shopping-cart.svg" alt="#" className='cart-icon'/>
+                    </div>
                 </div>
-
-                <button onClick={() => addToCompareBooks(id, 'compare')} className="book-comparison"
-                        type="submit"> </button>
+                <img className="card__pic" src={image}/>
             </div>
 
-            <div className="book-author--container">
-                <h3 className="book-author">{author}</h3>
-                <p className="book-price">{price}₴</p>
-
-            </div>
-
+            <h4 className="card__title">{title}</h4>
+            <p className="card__author">{author}</p>
+            <p className="card__price">{price} грн</p>
         </div>
 
-        
+
 
 
 
@@ -71,3 +55,33 @@ BookCard.defaultProps = {
 
 
 export default BookCard;
+{/*<div className="book-card">*/}
+
+{/*<div className="book-img__container">*/}
+{/*<img className="book-card__img"*/}
+{/*src={image}*/}
+{/*alt="#"/>*/}
+
+{/*/!*<img className="book-icon" src="/heart.svg" alt="#"/>*!/*/}
+{/*<h4 className="book-card__subtitle">{title}</h4>*/}
+{/*<img className="book-icon" src="/heart.svg" alt="#"/>*/}
+
+{/*<a className="book-link" href={webReaderLink} target='_blank'> </a>*/}
+
+
+{/*<div className="book-button--container">*/}
+{/*<button className="book-button" type="submit"> </button>*/}
+{/*<p className="book-add">Add To Cart</p>*/}
+{/*</div>*/}
+
+{/*<button onClick={() => addToCompareBooks(id, 'compare')} className="book-comparison"*/}
+{/*type="submit"> </button>*/}
+{/*</div>*/}
+
+{/*<div className="book-author--container">*/}
+{/*<h3 className="book-author">{author}</h3>*/}
+{/*<p className="book-price">{price}₴</p>*/}
+
+{/*</div>*/}
+
+{/*</div>*/}
