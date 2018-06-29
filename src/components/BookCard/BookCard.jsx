21bb8e20dd99f1e75d24842121a6f316a24ebf23
Michael <mike-l@ukr.net>
2018-06-29 18:17:01 +0300

    correct icon path

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -11,21 +11,21 @@ const BookCard = ({title, image, price, etag, author, toggleAddDeleteToArr, webR
 
                 <div className="card__overlay">
 
-                    <img className="book-icon" title='add to wish list' src="/like.svg" alt="#"
+                    <img className="book-icon" title='add to wish list' src="/WebShop/build/like.svg" alt="#"
                          onClick={() => toggleAddDeleteToArr(etag, 'wishList')}
                     />
 
-                    <img className="book-comparison" title='add to compaire' src="/balance.svg" alt="#"
+                    <img className="book-comparison" title='add to compaire' src="/WebShop/build/balance.svg" alt="#"
                          onClick={() => toggleAddDeleteToArr(etag, 'compare')}
                     />
                     <a className='preview' target={'_blank'} href={webReaderLink}>preview</a>
                     <div className="buy">
-                        <img src="/shopping-cart.svg" title='add to basket' alt="#" className='cart-icon'
+                        <img src="/WebShop/build/shopping-cart.svg" title='add to basket' alt="#" className='cart-icon'
                              onClick={() => toggleAddDeleteToArr(etag, 'basket')}
                         />
 
                         <NavLink to='/page-book'>
-                            <img className="book-info" title='go to books page' src="/icon.svg" alt="#"
+                            <img className="book-info" title='go to books page' src="/WebShop/build/icon.svg" alt="#"
                                  onClick={() => renderPage(etag)}
                             />
                         </NavLink>
