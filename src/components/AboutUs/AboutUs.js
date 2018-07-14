0716a4bd4a953504864b2e0b847fb336fa706d4f
Michael <mike-l@ukr.net>
2018-07-14 15:01:00 +0300

    aboutUs css

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -10,50 +10,51 @@ const AboutUs = (props) => {
 
                 <div id="wrapper" key={el.name}>
                     <div className="header-card">
-                    <img className='avatar' src={el.avatar} alt={el.name} />
-                </div>
-                <div className="info">
-                    <p>
-                        <i className="fa fa-bandcamp" aria-hidden="true"> </i>
-                        <span className="bold">{el.country}</span>
+                        <img className='avatar' src={el.avatar} alt={el.name}/>
+                    </div>
+                    <div className="info">
+                        <p>
+                            <i className="fa fa-bandcamp" aria-hidden="true"> </i>
+                            <span className="bold">{el.country}, </span>
 
                             <span className="level">{el.prof}</span>
-                    </p>
-                </div>
-                <div className="name">
-                    <h1 className='name-name'>
-                        {el.name}
-                    </h1>
-                    <div className="mline"/>
-                </div>
-                <ul className="contacts">
-                    <li>
-                        <a href={el.facebook}
-                           target='_blank'>
-                        <i className="fa fa-facebook" aria-hidden="true"/>
-                    </a>
-                    </li>
-                    <li>
-                        <a href={el.email}
-                           target='_blank'>
-                            <i className="fa fa-envelope-o" aria-hidden="true"/>
+                        </p>
+                    </div>
+                    <div className="name">
+                        <h1 className='name-name'>
+                            {el.name}
+                        </h1>
+                        <div className="mline"/>
+                    </div>
+                    <ul className="contacts">
+                        <li>
+                            <a href={el.facebook} title="go to Facebook page"
+                               target='_blank'>
+                                <i className="fa fa-facebook" aria-hidden="true" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href={el.linkedin} title="go to Linkedin page"
+                               target='_blank'>
+                                <i className="fa fa-linkedin" aria-hidden="true" />
+                                {/*<i className="fa fa-envelope-o" aria-hidden="true"/>*/}
+                            </a>
+                        </li>
+                    </ul>
+                    <div className="button">
+                        <a href={el.github}
+                           target='_blank'
+                           className="follow">
+                            GitHub
                         </a>
-                    </li>
-                </ul>
-                <div className="button">
-                    <a href={el.github}
-                       target='_blank'
-                       className="follow">
-                        GitHub
-                    </a>
-                </div>
-                
-                <div className="line"/>
-                <div className="line"/>
-                {/*<div className="footer">*/}
-                {/*<i className="fa fa-arrow-up" aria-hidden="true"></i>*/}
-                {/*</div>*/}
-            </div>)}
+                    </div>
+
+                    <div className="line"/>
+                    <div className="line"/>
+                    {/*<div className="footer">*/}
+                    {/*<i className="fa fa-arrow-up" aria-hidden="true"></i>*/}
+                    {/*</div>*/}
+                </div>)}
         </div>
     )
 };
